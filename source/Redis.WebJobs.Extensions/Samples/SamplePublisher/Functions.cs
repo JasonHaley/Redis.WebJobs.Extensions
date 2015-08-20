79fc6099fdd2518f71f7646ca8e83fc324c14aaf
Jason Haley <jason.haley@finomial.com>
2015-08-20 19:46:40 -0400

    Removed old samples

--- source/Redis.WebJobs.Extensions/Samples/SamplePublisher/Functions.cs
+++ source/Redis.WebJobs.Extensions/Samples/SamplePublisher/Functions.cs
@@ -43,26 +43,7 @@ namespace SamplePublisher
 
             log.WriteLine("sending Message: " + message.Id);
         }
-
-        //public static void SendSimpleMessage([RedisPublish("messages")] out string message, TextWriter log)
-        //{
-        //    message = "this is a test";
-
-        //    log.WriteLine("sending message: " + message);
-        //}
-
-        //public static void SendMessage([RedisPublish("messages")] out Message message, TextWriter log)
-        //{
-        //    message = new Message
-        //    {
-        //        Text = "message #",
-        //        Sent = DateTime.UtcNow,
-        //        Id = Guid.NewGuid()
-        //    };
-
-        //    log.WriteLine("sending Message: " + message.Id);
-        //}
-
+        
         public class Message
         {
             public Guid Id { get; set; }
