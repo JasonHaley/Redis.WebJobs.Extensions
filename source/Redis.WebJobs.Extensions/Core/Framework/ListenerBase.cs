080521651a5276589c8ce5f5c40ab94ca4f3ca2a
Jason Haley <jason@jasonhaley.com>
2015-08-24 08:09:56 -0400

    Missed files and removed keys from config ... again.

---
+++ source/Redis.WebJobs.Extensions/Core/Framework/ListenerBase.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Host.Executors;
+using Microsoft.Azure.WebJobs.Host.Listeners;
+using Redis.WebJobs.Extensions.Config;
+
+namespace Redis.WebJobs.Extensions.Framework
+{
+    internal abstract class ListenerBase : IListener
+    {
+        protected readonly CancellationTokenSource _cancellationTokenSource;
+        protected bool _disposed;
+        
+        public ListenerBase()
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+        }
+
+        protected virtual void OnDisposing()
+        {
+            
+        }
+        
+        [SuppressMessage("Microsoft.Usage", "CA2213:DisposableFieldsShouldBeDisposed", MessageId = "_cancellationTokenSource")]
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                _cancellationTokenSource.Cancel();
+
+                OnDisposing();
+                
+                _disposed = true;
+            }
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            ThrowIfDisposed();
+
+            OnStarting();
+           
+
+            return StartAsyncCore(cancellationToken);
+        }
+
+        protected abstract Task StartAsyncCore(CancellationToken cancellationToken);
+
+        protected virtual void OnStarting()
+        {
+            // override if needed
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            ThrowIfDisposed();
+
+            OnStopping();
+            
+            _cancellationTokenSource.Cancel();
+
+            return StopAsyncCore(cancellationToken);
+        }
+
+        protected abstract Task StopAsyncCore(CancellationToken cancellationToken);
+
+        protected virtual void OnStopping()
+        {
+            // override if needed
+        }
+
+        public void Cancel()
+        {
+            ThrowIfDisposed();
+            _cancellationTokenSource.Cancel();
+        }
+
+        protected void ThrowIfDisposed()
+        {
+            if (_disposed)
+            {
+                throw new ObjectDisposedException(null);
+            }
+        }
+    }
+}
---
+++ source/Redis.WebJobs.Extensions/Core/Listeners/CacheReceiver.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using Redis.WebJobs.Extensions.Config;
+using StackExchange.Redis;
+
+namespace Redis.WebJobs.Extensions.Listeners
+{
+    internal class CacheReceiver
+    {
+        private readonly RedisConfiguration _config;
+        private readonly string _channelOrKey;
+        private readonly string _lastValueKeyName;
+        public CacheReceiver(RedisConfiguration config, string channelOrKey, string lastValueKeyName)
+        {
+            _config = config;
+            _channelOrKey = channelOrKey;
+            _lastValueKeyName = lastValueKeyName;
+        }
+
+        public async Task OnExecuteAsync(Func<string, string, Task> processMessageAsync)
+        {
+            var connection = RedisClient.CreateConnectionFromConnectionString(_config.ConnectionString);
+            var db = connection.GetDatabase();
+
+            string prevValue = null;
+            string currentValue = null;
+
+            if (await db.KeyExistsAsync(_lastValueKeyName))
+            {
+                prevValue = await db.StringGetAsync(_lastValueKeyName, CommandFlags.None);
+            }
+
+            if (await db.KeyExistsAsync(_channelOrKey))
+            {
+                currentValue = await db.StringGetAsync(_channelOrKey, CommandFlags.None);
+            }
+
+            bool hadValue = !string.IsNullOrEmpty(prevValue);
+            bool hasValue = !string.IsNullOrEmpty(currentValue);
+            if ((hadValue || hasValue) && currentValue != prevValue)
+            {
+                // set value for comparison next check
+                db.StringSet(_lastValueKeyName, currentValue);
+
+                // process the value since it has changed
+                await processMessageAsync(prevValue, currentValue);
+            }
+        }
+    }
+}
