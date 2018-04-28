6dbf3cc16c998d84fc31f485e30ffc546e079ff6
Jason Haley <jason@jasonhaley.com>
2018-04-28 12:31:11 -0400

    got the function app working

---
+++ source/Redis.WebJobs.Extensions/Samples/FunctionApp/PublisherFunction.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+using Redis.WebJobs.Extensions;
+
+namespace FunctionApp
+{
+    public static class PublisherFunction
+    {
+        [FunctionName("PublisherFunction")]
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req,
+            [Redis("pubsub:simpleMessages", Mode.PubSub)] IAsyncCollector<string> messages, TextWriter log)
+        {
+            string message = req.Query["message"];
+
+            string requestBody = new StreamReader(req.Body).ReadToEnd();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            message = message ?? data?.message;
+
+            messages.AddAsync(message);
+
+            log.WriteLine($"Sending message: {message}");
+
+            return message != null
+                ? (ActionResult)new OkObjectResult($"Message sent: {message}")
+                : new BadRequestObjectResult("Please pass a message on the query string or in the request body");
+        }
+    }
+}
